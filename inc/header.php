84989045121f9d6ecd3e6976c30c322dad9011a8
NguyenHoangVen <nguyenhoangven92@gmail.com>
2021-01-26 13:06:38 +0700

    d

--- inc/header.php
+++ inc/header.php
@@ -1,26 +1,26 @@
 <div id="header">
 	<div class="wp-inner clearfix">
-		<a href="home.php" id="logo" class="fl-left"><img src="public/images/logo.png" alt="Logo" title="Trang chủ"></a>
+		<a href="home.html" id="logo" class="fl-left"><img src="public/images/logo.png" alt="Logo" title="Trang chủ"></a>
 		<ul id="main-menu" class="fl-left">
-			<li><a href="home.php">Trang chủ</a></li>
-			<li><a href="home.php">Về chúng tôi</a></li>
-			<li><a class="angle-down" href="category.php">Sản phẩm</a>
+			<li><a href="home.html">Trang chủ</a></li>
+			<li><a href="home.html">Về chúng tôi</a></li>
+			<li><a class="angle-down" href="category.html">Sản phẩm</a>
 				<ul class="sub-menu">
-					<li><a href="detail.php">Mùa xuân</a></li>
-					<li class="active"><a class="angle-right" href="detail.php">Mùa hạ</a>
+					<li><a href="detail.html">Mùa xuân</a></li>
+					<li class="active"><a class="angle-right" href="detail.html">Mùa hạ</a>
 						<ul class="sub-menu">
-							<li><a href="detail.php">Áo vải lụa</a></li>
-							<li><a href="detail.php">Quần form rộng</a></li>
-							<li><a href="detail.php">Đồ bơi</a></li>
+							<li><a href="detail.html">Áo vải lụa</a></li>
+							<li><a href="detail.html">Quần form rộng</a></li>
+							<li><a href="detail.html">Đồ bơi</a></li>
 						</ul>
 					</li>
-					<li><a href="detail.php">Mùa thu</a></li>
-					<li><a href="detail.php">Mùa đông</a></li>
+					<li><a href="detail.html">Mùa thu</a></li>
+					<li><a href="detail.html">Mùa đông</a></li>
 				</ul>
 			</li>
-			<li><a href="category.php">Xu hướng</a></li>
-			<li><a href="category.php">Thanh toán</a></li>
-			<li><a href="category.php">Liên hệ</a></li>
+			<li><a href="category.html">Xu hướng</a></li>
+			<li><a href="category.html">Thanh toán</a></li>
+			<li><a href="category.html">Liên hệ</a></li>
 		</ul>
 		<div id="widget-search" class="fl-left">
 			<i id="icon-search" class="fa fa-search" aria-hidden="true"></i>
